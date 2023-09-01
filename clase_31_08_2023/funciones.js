f289f86828df067dfa5036532deddff770a1b3f4
Christopher Moyano <cmoyano@ing.ucsc.cl>
2023-09-01 11:49:46 -0400

    clase_01_09_2023

--- clase_31_08_2023/funciones.js
+++ clase_31_08_2023/funciones.js
@@ -9,8 +9,25 @@ function azul() {
     document.getElementById('azul').style.backgroundColor = 'blue';
 }
 function verde() {
-    document.getElementsByClassName("rojo")[0].style.backgroundColor = 'green';   
+    for (let index = 0; index < 5; index++) {
+        document.getElementsByClassName("rojo")[index].style.backgroundColor = 'green';
+        
+    }
+    //document.getElementsByClassName("rojo")[0].style.backgroundColor = 'green';   
 }
 function rojo(){
-    document.getElementById('azul').style.backgroundColor = 'red';
+    for (let index = 0; index < 5; index++) {
+        document.getElementsByClassName("rojo")[index].style.backgroundColor = 'red';
+        
+    }
+}
+function mostrar(){
+    var primero =$("#primero").val();
+    var segundo = $("#segundo").val();
+    console.log("los numeros son: "+primero+" y "+segundo);
+}
+function sumar() {
+    var primero =parseInt($("#primero").val());
+    var segundo = parseInt($("#segundo").val());
+    console.log("la suma de los numeros es: "+ parseInt(primero+segundo) )
 }
